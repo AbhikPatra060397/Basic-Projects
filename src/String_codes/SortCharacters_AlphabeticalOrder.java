ad3941a16c1ee6edce02ccba5f42e3418b3c25ba
AbhikPatra060397 <abhik.patra2021@gmail.com>
2024-05-20 02:00:17 +0530

    String Codes 1. SortCharacters_AlphabeticalOrder

---
+++ src/String_codes/SortCharacters_AlphabeticalOrder.java
@@ -0,0 +1,32 @@
+package String_codes;
+
+import java.util.Arrays;
+
+public class SortCharacters_AlphabeticalOrder {
+    public static void main(String[] args) {
+        String str = "dkcab";
+
+        // without sort method
+        char[] ch = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<str.length();i++){
+            for (int j=i+1;j<str.length();j++){
+                while (ch[i]>ch[j]) {
+                    char temp = str.charAt(i);
+                    ch[i]=ch[j];
+                    ch[j]= temp;
+                }
+            }
+        }
+        /*for (char c : ch) {
+            System.out.print(c);+
+        }*/
+        System.out.println(new String(ch));
+
+
+        // with sort method
+        char[] ch1 = str.toCharArray();
+        Arrays.sort(ch1);
+        System.out.println(new String(ch1));
+    }
+}
