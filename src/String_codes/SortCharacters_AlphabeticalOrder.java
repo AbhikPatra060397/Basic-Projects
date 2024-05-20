73eadba400d12c6955e4248f9d8e019e984b03f9
AbhikPatra060397 <abhik.patra2021@gmail.com>
2024-05-20 23:51:35 +0530

    String Codes 1. ReplaceCharacter_WithOccurrence

--- src/String_codes/SortCharacters_AlphabeticalOrder.java
+++ src/String_codes/SortCharacters_AlphabeticalOrder.java
@@ -9,20 +9,16 @@ public class SortCharacters_AlphabeticalOrder {
         // without sort method
         char[] ch = str.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for (int i=0;i<str.length();i++){
-            for (int j=i+1;j<str.length();j++){
-                while (ch[i]>ch[j]) {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                while (ch[i] > ch[j]) {
                     char temp = str.charAt(i);
-                    ch[i]=ch[j];
-                    ch[j]= temp;
+                    ch[i] = ch[j];
+                    ch[j] = temp;
                 }
             }
         }
-        /*for (char c : ch) {
-            System.out.print(c);+
-        }*/
         System.out.println(new String(ch));
-
 
         // with sort method
         char[] ch1 = str.toCharArray();
